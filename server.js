7d2cf7740b57961f61c454eb4d29ef6e181fa63a
Aleksey Kovalchuk <kovalchuk.aleksey.s@gmail.com>
2022-02-07 11:01:49 +0300

    Add cors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+// Enable CORS
+var cors = require('cors')
 
+app.use(cors())
 // parse application/json
 app.use(require('body-parser').json());
 
